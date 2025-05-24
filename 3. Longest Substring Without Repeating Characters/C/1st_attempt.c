7598ad33ff992503ff7e758783bb9f3d88dbb4a0
emilioq1 <quiemili354@gmail.com>
2025-05-23 19:45:32 -0500

    added implementations of 3. Longest Substring Without Repeating Characters

--- 3. Longest Substring Without Repeating Characters/C/1st_attempt.c
+++ 3. Longest Substring Without Repeating Characters/C/1st_attempt.c
@@ -31,43 +31,31 @@ int lengthOfLongestSubstring(char* s) {
     }
 
     for(int i = 0; i < s_len; ++i) {
-        printf("current best: %s\n", best_str);
-        printf("temp: %s\n", temp_str);
         temp_str_len = strnlen(temp_str, MAX_STR_LEN);
         best_str_len = strnlen(best_str, MAX_STR_LEN);
         for(int j = 0; j < temp_str_len; ++j) {
-            //printf("comparing: \n");
-            //printf("temp_str[%d] = %c\n", j, temp_str[j]);
-            //printf("s[%d] = %c\n\n", i, s[i]);
-
-            if(temp_str[j] == s[i] || s[i] == '\0') {
-                //printf("temp_str length: %d\n", temp_str_len);
-                //printf("best_str length: %d\n", best_str_len);
-
+            if(temp_str[j] == s[i]) {
                 if(temp_str_len > best_str_len) {
                     strncpy(best_str, temp_str, MAX_STR_LEN);
                 }
+
                 memset(temp_str, 0, sizeof(temp_str));
                 temp_str_index = 0;
                 i = starting_index;
                 ++starting_index;
+
                 break;
             }
 
         }
         temp_str[temp_str_index] = s[i];
-
         ++temp_str_index;
     }
 
-    // if(strnlen(best_str, MAX_STR_LEN) == 0 || strnlen(temp_str, MAX_STR_LEN) > strnlen(best_str, MAX_STR_LEN)) {
-    if(best_str_len == 0 || temp_str > best_str) {
+    if(strnlen(best_str, MAX_STR_LEN) == 0 || strnlen(temp_str, MAX_STR_LEN) > strnlen(best_str, MAX_STR_LEN)) {
         strncpy(best_str, temp_str, MAX_STR_LEN);
     }
     
-    printf("final temp_str: %s\n", temp_str);
-    printf("final best_str: %s\n", best_str);
-
     return strnlen(best_str, MAX_STR_LEN);
 }
 
@@ -79,7 +67,6 @@ int main(void) {
 
     printf("test_str: \"%s\"\n", test_str);
     printf("result: %d\n", result);
-
 
     return 0;
 }
