02907678fcc6ed97a553d5decd38c28103b12f1b
emilioq1 <quiemili354@gmail.com>
2026-01-20 13:34:45 -0600

    I used a long in my first attempt which was not allowed, so I am refactoring it to not use a long

--- 7. Reverse Integer/C/first_attempt.c
+++ 7. Reverse Integer/C/first_attempt.c
@@ -11,7 +11,7 @@
 int reverse(int x) {
 	char buffer[MAX_LENGTH + 1] = {0};
 
-	int numChar = snprintf(buffer, MAX_LENGTH, "%d", x);
+	int32_t numChar = snprintf(buffer, MAX_LENGTH, "%d", x);
 	if(numChar < 0) {
 		return 0;
 	}
@@ -24,24 +24,48 @@ int reverse(int x) {
 	printf("buffer: %s\n", buffer);
 
 	char reverse[MAX_LENGTH + 1] = {0};
-	int i = 0;
-	int k = 0;
+	int8_t i = 0;
+	int8_t k = 0;
 
 	if(buffer[0] == '-') {
 		reverse[i++] = '-';
 		++k;
 	}
 
-	for(int j = MAX_LENGTH; j >= k; --j) {
+	for(int8_t j = MAX_LENGTH; j >= k; --j) {
 		printf("buffer[%d]: %c\n", j, buffer[j]);
 		if(buffer[j] != 0) reverse[i++] = buffer[j];
 	}
-	buffer[MAX_LENGTH] = '\0';
+	reverse[i] = '\0';
+
+	int32_t length = strnlen(reverse, MAX_LENGTH + 1);
+	printf("length: %d\n", length);
 
 	printf("reverse: %s\n", reverse);
 
+	int32_t result = 0;
+
+	if(reverse[0] != '-') {
+		if((int)reverse[0] > 50 && length > 10) {
+			return result;
+		}
+	}
+	else {
+		if((int)reverse[1] > 50 && length > 10) {
+			return result;
+		}
+	}
+
+	char* endptr;
+
+	/*
+	 * 9646324351 base-10 = 10 00111110 11110111 00111010 01111111 base-2
+	 * 1056389759 base-10 = 00 00111110 11110111 00111010 01111111 base-2
+	 */
+
 	errno = 0;
-	long result = strtoll(reverse, NULL, 10);
+	result = strtol(reverse, &endptr, 10);
+	printf("errno: %d\n", errno);
 	if(errno == EINVAL) {
 		perror("strtol");
 		exit(EXIT_FAILURE);
@@ -49,6 +73,17 @@ int reverse(int x) {
 	else if(errno == ERANGE) {
 		perror("strtol");
 		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", INT32_MAX);
+
+	if(endptr == reverse) {
+		printf("No digits were found.\n");
+	}
+	else if(*endptr != '\0') {
+		printf("Invalid character: %c\n", *endptr);
+	}
+	else {
+		printf("The number is: %d\n", result);
 	}
 
 	if(result < -2147483648 || result > 2147483647) {
