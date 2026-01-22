e9b28579ecda583b33cc6891c0dd9415b1398001
emilioq1 <quiemili354@gmail.com>
2026-01-21 20:56:00 -0600

    My blind attempt got accepted on Leetcode

--- 8. String to Integer (atoi)/C/blind_attempt.c
+++ 8. String to Integer (atoi)/C/blind_attempt.c
@@ -2,6 +2,7 @@
 
 #include <ctype.h>
 #include <math.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -9,122 +10,150 @@
 
 int myAtoi(char* s) {
 	// temp variable
-	char c = ' ';
+	char curr = ' ';
+	char prev = 0;
 
 	// -1 -> negative
 	// 0 -> unset
 	// 1 -> positive
 	int sign = 0;
-	// 1 when you have parsed a digit
-	// 0 when you haven't parsed a digit
-	int inNumBlock = 0;
-	// counts the number of zeroes that are parsed
-	// Used as an offset to get rid of leading zeroes in fs
-	int zeroCounter = 0;
+	// true -> when you have parsed a digit
+	// false -> when you haven't parsed a digit
+	bool numParsed = false;
+	// -1 -> indication to stop counting zeroes
+	// - Counts the number of leading zeroes that are parsed
+	// - Used as an offset to get rid of leading zeroes in fs
+	int leadingZeroCount = 0;
 
 	// Formatted string
 	char fs[MAX_LENGTH] = {0};
 	int fsLen = 0;
 
 	int i = 0;
-	while(c != '\0') {
-		printf("s[%d]: %c\n", i, c);
-		c = s[i];
+	while(curr != '\0') {
+		// printf("curr[%d]: %c\n", i, curr);
 
-		if(c == ' ') {
-			printf("is space\n");
-			if(inNumBlock) {
+		curr = s[i];
+
+		if(curr == ' ') {
+			// printf("is space\n");
+			//  Stop parsing if a digit was parsed before
+			if(numParsed || sign != 0) {
 				break;
 			}
 		}
-		if(c == '-') {
-			if(inNumBlock || sign != 0) {
+		if(curr == '-') {
+			// printf("is negative\n");
+			//  Stop parsing if a digit or sign was parsed before
+			if(numParsed || sign != 0) {
 				break;
 			}
-			printf("is negative\n");
 			sign = -1;
 		}
-		if(c == '+') {
-			if(inNumBlock || sign != 0) {
+		if(curr == '+') {
+			// printf("is positive\n");
+			// Stop parsing if a digit or sign was parsed before
+			if(numParsed || sign != 0) {
 				break;
 			}
-			printf("is positive\n");
 			sign = 1;
 		}
-		if(isdigit(c)) {
-			printf("is digit\n");
-			if(c == '0') ++zeroCounter;
-			if(zeroCounter == fsLen && c != '0') {
-				fsLen -= zeroCounter;
-				zeroCounter = 0;
-				fs[fsLen] = c;
-				inNumBlock = 1;
-				printf("fs[%d]: %c\n", fsLen, fs[fsLen]);
-				++fsLen;
+		if(isdigit(curr)) {
+			// printf("is digit\n");
+			//  if(curr == '0' && leadingZeroCount != -1) ++leadingZeroCount;
+
+			// Stop counting leading zeroes when you parse a digit
+			// if(leadingZeroCount > 0 && curr != '0') {
+			//	fsLen -= leadingZeroCount;
+			//	leadingZeroCount = -1;
+			//}
+			//
+
+			/**
+			 * 1. prev == -1 && curr = digit =>  fs[fsLen] = curr;
+			 * 2. leadZeroBlock && prev == '0' && curr == '0' => {
+			 *      fsLen -= 1;
+			 *      leadZeroBlock = true;
+			 *    }
+			 * 3. leadZeroBlock && prev == '' && curr == '0' => {
+			 *      WOULD NEVER HAPPEN
+			 *    }
+			 * 4. leadZeroBlock && prev == '0' && curr == '' => {
+			 *          fsLen -= 1;
+			 *         leadZeroBlock = false;
+			 *    }
+			 *
+			 * 5. !leadZeroBlock && prev == '0' && curr == '0' =>
+			 *      nothing
+			 * 6. !leadZeroBlock && prev == '' && curr == '0' =>
+			 * 7. !leadZeroBlock && prev == '0' && curr == '' =>
+			 * */
+
+			// Cases:
+			// - First two parsed digits are '0'.
+			// - prev == '0' and curr == '0' in a leading zero block.
+			if(prev == '0' && curr == '0' && leadingZeroCount != -1) {
+				fsLen -= 1;
+				++leadingZeroCount;
 			}
-			else {
-				fs[fsLen] = c;
-				printf("fs[%d]: %c\n", fsLen, fs[fsLen]);
-				inNumBlock = 1;
-				++fsLen;
+			// Case: In a leading zero block, a non-zero digit is parsed.
+			else if(prev == '0' && curr != '0' && leadingZeroCount != -1) {
+				fsLen -= 1;
+				leadingZeroCount = -1;
 			}
+			else if(curr != '0' && leadingZeroCount <= 0) {
+				leadingZeroCount = -1;
+			}
+
+			fs[fsLen] = curr;
+			numParsed = true;
+			// printf("fs[%d]: %c\n", fsLen, fs[fsLen]);
+			++fsLen;
 		}
-		if(isalpha(c) || c == '.') {
-			printf("is alpha\n");
+		if(isalpha(curr) || curr == '.') {
+			// Always stop parsing if alphabetic character is parsed
+			// printf("is alpha\n");
 			break;
 		}
 
+		prev = s[i];
 		++i;
 	}
-	fs[fsLen] = '\0';
 
 	if(sign == 0) sign = 1;
 
 	printf("fs: %s\n", fs);
-
-	/*
-	 * Oct   Dec   Hex   Char
-	 * ───────────────────────
-	 * 060   48    30    0
-	 * 061   49    31    1
-	 * 062   50    32    2
-	 * 063   51    33    3
-	 * 064   52    34    4
-	 * 065   53    35    5
-	 * 066   54    36    6
-	 * 067   55    37    7
-	 * 070   56    38    8
-	 * 071   57    39    9
-	 */
+	printf("fsLen: %d\n", fsLen);
+	// printf("leadingZeroCount: %d\n", leadingZeroCount);
 
 	long result = 0;
 
-	if(fsLen > 10) {
-		if(sign == -1) return INT32_MIN;
-		else return INT32_MAX;
-	}
-
 	for(int i = 0; i < fsLen; ++i) {
-		int32_t num = (fs[i] - '0');
-		int32_t place = (int32_t)pow(10, (fsLen - i - 1));
+		int32_t num = fs[i] - '0';
+		int32_t power = fsLen - i - 1;
+
+		if((power == 9 && num > 2) || power > 9) {
+			return (sign == -1) ? INT32_MIN : INT32_MAX;
+		}
+
+		int32_t place = pow(10, power);
 		long final = num * place;
 		// printf("fs[%d]: %c\n", i, fs[i]);
 		// printf("temp: %d\n", temp);
 		// printf("powM: %d\n", powM);
 		printf("final: %ld\n", final);
 		result += final;
-		if(result * sign > INT32_MAX) {
-			return INT32_MAX;
-		}
-		else if(result * sign < INT32_MIN) {
-			return INT32_MIN;
+
+		if((result * sign) > INT32_MAX || (result * sign) < INT32_MIN) {
+			return (sign == -1) ? INT32_MIN : INT32_MAX;
 		}
 	}
+
 
 	return result * sign;
 }
 
-#define TEST_CASES 8
+#define TEST_CASES 10
 
 int main() {
 	char* cases[TEST_CASES] = {"42",
@@ -134,7 +163,9 @@ int main() {
 							   "words and 987",
 							   "  0000000000012345678",
 							   "+-12",
-							   "    0000000000000   "};
+							   "    0000000000000   ",
+							   "2147483800",
+							   "-6147483648"};
 
 	printf("---------------------------------------------------------\n\n");
 
