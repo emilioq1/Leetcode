83dcac44774dc77c2d143134b5171be544a95bc4
emilioq1 <quiemili354@gmail.com>
2026-01-25 02:51:51 -0600

    Blind attempt for question 10 still a work in progress

--- 10. Regular Expression Matching/C/blind_attempt.c
+++ 10. Regular Expression Matching/C/blind_attempt.c
@@ -1,6 +1,5 @@
 #define _POSIX_C_SOURCE 200809L
 
-#include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,78 +7,151 @@
 #define PRINT(x) x ? x : 'N'
 
 bool isMatch(char* s, char* p) {
-	if(!strncmp(p, ".*", 21)) return true;
-	if(!strncmp(s, p, 21)) return true;
+	if(strcmp(p, ".*") == 0) return true;
+	if(strcmp(p, s) == 0) return true;
 
-	size_t sLen = strnlen(s, 21);
-
-	// p consists of only '.' (any character) and is same length of s
-	if(strspn(p, ".") == sLen) return true;
-
+	size_t sLen = strnlen(s, 20);
 	size_t pLen = strnlen(p, 21);
 
-
-	printf("sLen: %ld\n", sLen);
-	printf("pLen: %ld\n\n", pLen);
+	printf("sLen: %lu\n", sLen);
+	printf("pLen: %lu\n", pLen);
 
 	int i = 0;
-	int j = 0;
+
+	int starCount = 0;
+	// Note: If a letter is next to a star, it is not counted in
+	// letterOrDotCount as there can be zero or more occurances of the letter.
+	// int letterOrDotCount = 0;
+	int letterCount = 0;
+	int dotCount = 0;
+
+	for(int k; k < pLen; ++k) {
+		char next = p[k + 1];
+
+		if(p[k] == '.') {
+			if(next != '*') {
+				++dotCount;
+			}
+		}
+		else if(p[k] == '*') {
+			++starCount;
+		}
+		else {
+			if(next != '*') {
+				++letterCount;
+			}
+		}
+
+		// At end of s
+		if(next == '\0') break;
+	}
+	int letterOrDotCount = letterCount + dotCount;
+
+	printf("letterOrDotCount: %d\n", letterOrDotCount);
+	printf("starCount: %d\n", starCount);
+
+	if(starCount == 0 && letterOrDotCount < sLen) return false;
+	if(starCount == 1 && letterOrDotCount == 0 && s[0] != p[0]) return false;
+	if(letterOrDotCount > sLen) return false;
 
 	char prev = '\0';
-	char curr = p[j];
+	char curr = p[0];
 	char next = '\0';
 
-	while(curr != '\0') {
-		next = p[++j];
+	printf("--------------------------------\n");
 
-		printf("i: %d, j: %d\n", i, j);
-		printf("s[i]: %c\n", s[i]);
-		printf("curr: %c, next: %c\n\n", PRINT(curr), PRINT(next));
+	bool ignoreMatch = false;
+
+	for(int j = 0; j < pLen; ++j) {
+		next = p[j + 1];
+
+		printf("j: %d\n", j);
+		printf("\tletterCount = %d\n", letterCount);
+		printf("\tdotCount = %d\n", dotCount);
+		printf("\tstarCount = %d\n", starCount);
+		printf("\ts = \"%s\"\n", s);
+		printf("\tp = \"%s\"\n", p);
+		printf("\t\ti = %d\t\tj = %d\n", i, j);
+		printf("\ts[%d] =\t%c\tp[%d] =\t%c\n", i, PRINT(s[i]), j, PRINT(p[j]));
+		printf("\tcurr =\t%c\tnext =\t%c\n\n", PRINT(curr), PRINT(next));
 
 		switch(curr) {
 			case '.':
-				printf(".\n\n");
+				printf("  .\n");
+				if(next == '*') {
+					prev = curr;
+					printf("\tstar is next, break\n\n");
+					break;
+				}
 				++i;
+				//--letterOrDotCount;
+				--dotCount;
 				break;
 			case '*':
-				printf("*\n");
+				printf("  *\n");
+				--starCount;
 
-				int remainingP = pLen - j;
-				printf("remainingP: %d\n", remainingP);
+				// int remainingP = pLen - j;
+				// printf("remainingP: %d\n", remainingP);
+				printf("\tInitial letterOrDotCount: %d\n", letterOrDotCount);
+				printf("\ts[%d] = %c\n\n", i, PRINT(s[i]));
 
-				while(s[i] == prev || prev == '.') {
+				if(prev == '.') ignoreMatch = true;
+
+				while(s[i] == prev || (prev == '.' && s[i] != next)) {
 					int remainingS = sLen - i;
-					printf("remainingS: %d\n", remainingS);
+					printf("\tremainingS: %d\n", remainingS);
+					printf("\tletterCount: %d\n", letterCount);
+					printf("\tletterOrDotCount: %d\n", letterOrDotCount);
 
-					if(remainingS == remainingP) break;
+					if(remainingS == letterCount) {
+						printf("\tremainingS == letterOrDotCount\n");
+						break;
+					}
+					if(remainingS == 0) {
+						printf("\tremainingS == 0\n");
+						break;
+					}
 					++i;
 				}
+				printf("\n");
+				printf("\tFinal remainingS: %lu\n", sLen - i);
+				printf("\tFinal letterOrDotCount: %d\n", letterOrDotCount);
 
 				printf("\n");
 				break;
 			default:
-				printf("letter\n");
+				printf("  letter\n");
 
 				if(next == '*') {
 					prev = curr;
-					printf("star is next\n\n");
+					printf("\tstar is next, break\n\n");
 					break;
 				}
 
 
 				if(s[i] == curr) {
-					printf("match\n\n");
+					printf("\tmatch\n\n");
+					//--letterOrDotCount;
+					--letterCount;
 					++i;
 					break;
 				}
 				else {
-					printf("no match\n\n");
-					return false;
+					printf("\tno match\n\n");
+					if(ignoreMatch == false) return false;
 				}
+				break;
 		}
+		printf("--------------------------------\n\n");
+
+		letterOrDotCount = letterCount + dotCount;
+
+		if(letterOrDotCount == 0 && starCount == 0) break;
 
 		// At end of s
 		if(next == '\0') break;
+		if(s[i] == '\0') break;
 
 		curr = next;
 	}
@@ -91,13 +163,33 @@ bool isMatch(char* s, char* p) {
 	return true;
 }
 
-#define TEST_CASES 9
+#define TEST_CASES 11
 
 int main() {
-	char* s[TEST_CASES] = {"aa", "aa",			"ab",  "aab", "abcd",
-						   "ab", "mississippi", "aaa", "aaa"};
-	char* p[TEST_CASES] = {"a",	  "a*",			 ".*",	"c*a*b",   "d*",
-						   ".*c", "mis*is*ip*.", "a*a", "ab*a*c*a"};
+	char* s[TEST_CASES] = {"aa",
+						   "aa",
+						   "ab",
+						   "aab",
+						   "abcd",
+						   "ab",
+						   "mississippi",
+						   "aaa",
+						   "aaa",
+						   "mississippi",
+						   "aabcbcbcaccbcaabc"};
+	char* p[TEST_CASES] = {"a",
+						   "a*",
+						   ".*",
+						   "c*a*b",
+						   "d*",
+						   ".*c",
+						   "mis*is*ip*.",
+						   "a*a",
+						   "ab*a*c*a",
+						   "mis*is*p*.",
+						   ".*a*aa*.*b*.c*.*a*"};
+	bool results[TEST_CASES] = {false, true, true, true,  false, false,
+								true,  true, true, false, true};
 
 
 	printf("---------------------------------------------------------\n\n");
@@ -106,10 +198,16 @@ int main() {
 		printf("TEST CASE:\n");
 		printf("\ts: \"%s\"\n", s[i]);
 		printf("\tp: \"%s\"\n\n", p[i]);
-		int result = isMatch(s[i], p[i]);
+		bool result = isMatch(s[i], p[i]);
 		printf("result: %s\n\n", result ? "true" : "false");
+		if(results[i] != result) {
+			printf(
+				"---------------------TEST CASE FAILED---------------------\n");
+			break;
+		}
 
-		printf("---------------------------------------------------------\n\n");
+		printf("---------------------------------------------------------"
+			   "\n\n");
 	}
 
 	return 0;
