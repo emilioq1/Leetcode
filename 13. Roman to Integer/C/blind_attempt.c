e7796d19bce6b6fb6921295aa2ef87c599b006f4
emilioq1 <quiemili354@gmail.com>
2026-02-10 14:03:40 -0600

    Finished blind attempt C code for question 13 and it was accepted in leetcode

--- 13. Roman to Integer/C/blind_attempt.c
+++ 13. Roman to Integer/C/blind_attempt.c
@@ -1,9 +1,8 @@
-#define GNU11 1
 #define _GNU_SOURCE
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 #define ANSI_COLOR_RED "\x1b[31m"
 #define ANSI_COLOR_GREEN "\x1b[32m"
@@ -11,7 +10,72 @@
 
 #define MAX_LENGTH 15
 
-int romanToInt(char* s) { return 0; }
+int romanToInt(char* s) {
+	size_t sLen = strnlen(s, MAX_LENGTH);
+	int i = 0;
+	int result = 0;
+
+	for(int i = 0; i < sLen; ++i) {
+		switch(s[i]) {
+			case 'I':
+				if(s[i + 1] == 'V') {
+					result += 4;
+					++i;
+				}
+				else if(s[i + 1] == 'X') {
+					result += 9;
+					++i;
+				}
+				else {
+					result += 1;
+				}
+				break;
+			case 'V':
+				result += 5;
+				break;
+			case 'X':
+				if(s[i + 1] == 'L') {
+					result += 40;
+					++i;
+				}
+				else if(s[i + 1] == 'C') {
+					result += 90;
+					++i;
+				}
+				else {
+					result += 10;
+				}
+				break;
+			case 'L':
+				result += 50;
+				break;
+			case 'C':
+				if(s[i + 1] == 'D') {
+					result += 400;
+					++i;
+				}
+				else if(s[i + 1] == 'M') {
+					result += 900;
+					++i;
+				}
+				else {
+					result += 100;
+				}
+				break;
+			case 'D':
+				result += 500;
+				break;
+			case 'M':
+				result += 1000;
+				break;
+			default:
+				break;
+		}
+	}
+
+
+	return result;
+}
 
 #define TEST_CASES 3
 
@@ -20,7 +84,6 @@ int main() {
 	int results[TEST_CASES] = {3, 58, 1994};
 
 	int i = 0;
-
 
 	printf("---------------------------------------------------------\n\n");
 
