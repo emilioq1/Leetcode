25fdee49775b551e25a5cde720e19f6f6fe1cf62
emilioq1 <quiemili354@gmail.com>
2026-02-09 22:22:40 -0600

    Created and finished best solution code for question 11 and it was accepted in leetcode

---
+++ 11. Container With Most Water/C/best_solution.c
@@ -0,0 +1,168 @@
+/**
+ * My solution improved from looking at solutions in Leetcode.
+ *
+ * Time complexity: O(N)
+ * Space complexity: O(1)
+ */
+
+#if defined(__STDC_VERSION__) && (__STDC_VERSION__ == 201112L) && !defined(__STRICT_ANSI__)
+#define GNU11 1
+#endif
+
+#define _GNU_SOURCE
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 100000
+#define MAX_HEIGHT 10000
+
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+
+typedef struct {
+	int* arr;
+	size_t size;
+} IntArray;
+
+IntArray createCase(char* filename) {
+	FILE* fp = fopen(filename, "r");
+	fseek(fp, 1, SEEK_SET);
+	int* resultArr = calloc(1, sizeof(int));
+
+	IntArray result = {0};
+
+	char* line = NULL;
+	size_t size = 0;
+	ssize_t nread = getline(&line, &size, fp);
+
+	if(nread == -1) {
+		free(line);
+		return result;
+	}
+
+	char* token = strtok(line, ",");
+
+	int i = 0;
+
+	while(token != NULL) {
+		int num = strtol(token, NULL, 10);
+		resultArr[i++] = num;
+		resultArr = realloc(resultArr, ((i + 1) * sizeof(int)));
+		// printf("%d\n", num);
+		token = strtok(0, ",");
+	}
+
+	result.arr = resultArr;
+	result.size = i;
+
+	free(line);
+
+	return result;
+}
+
+int maxArea(int* height, int heightSize) {
+	if(heightSize == 2) {
+		return MIN(height[0], height[1]);
+	}
+
+	// n == height.length
+	// 2 <= n <= 10^5
+	// 0 <= height[i] <= 10^4
+	int maxPossibleArea = heightSize * MAX_HEIGHT;
+	int currentMax = 0;
+
+	int left = 0;
+	int right = heightSize - 1;
+
+	while(left < right) {
+		int leftHeight = height[left];
+		int rightHeight = height[right];
+
+		if(leftHeight != 0 && rightHeight != 0) {
+			int width = right - left;
+			int height = MIN(leftHeight, rightHeight);
+
+			if(width == 1 && height > currentMax) {
+				currentMax = height;
+			}
+			else if(height == 1 && width > currentMax) {
+				currentMax = width;
+			}
+			else {
+				int area = width * height;
+				if(area == maxPossibleArea) return area;
+				if(area > currentMax) {
+					currentMax = area;
+				}
+			}
+		}
+
+		if(leftHeight < rightHeight) ++left;
+		else --right;
+	}
+
+	return currentMax;
+}
+
+#define TEST_CASES 2
+
+void printArray(int* arr, int size) {
+	printf("[");
+	for(int i = 0; i < size; ++i) {
+		if(i + 1 == size) printf("%d", arr[i]);
+		else printf("%d, ", arr[i]);
+	}
+	printf("]\n");
+}
+
+int main() {
+	int cases[TEST_CASES][MAX_HEIGHT] = {
+		{1, 8, 6, 2, 5, 4, 8, 3, 7},
+		{1, 1},
+	};
+	int sizes[TEST_CASES] = {9, 2};
+	int results[TEST_CASES] = {49, 1};
+
+	int i = 0;
+
+
+	printf("---------------------------------------------------------\n\n");
+
+	for(i = 0; i < TEST_CASES; ++i) {
+		printf("TEST CASE:\n");
+		printf("\tcase %d: ", i);
+		printArray(cases[i], sizes[i]);
+
+		int result = maxArea(cases[i], sizes[i]);
+		printf("result: %d\n\n", result);
+
+		if(results[i] != result) {
+			printf("---------------------TEST CASE FAILED---------------------\n");
+			break;
+		}
+
+		printf("---------------------------------------------------------"
+			   "\n\n");
+	}
+
+	IntArray case3 = createCase("case.txt");
+	printf("TEST CASE:\n");
+	printf("\tcase %d: ", i);
+	printArray(case3.arr, case3.size);
+
+	int result = maxArea(case3.arr, case3.size);
+	printf("result: %d\n\n", result);
+
+	if(705634720 != result) {
+		printf("---------------------TEST CASE FAILED---------------------\n");
+	}
+
+	printf("---------------------------------------------------------"
+		   "\n\n");
+
+	free(case3.arr);
+
+
+	return 0;
+}
