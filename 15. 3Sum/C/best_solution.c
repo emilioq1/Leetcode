2d2fca2b229803fed678ee75a164b7658c3827ee
emilioq1 <quiemili354@gmail.com>
2026-02-27 15:29:56 -0600

    Finished the best solution implementation for question 15 and it was accepted on Leetcode

--- 15. 3Sum/C/best_solution.c
+++ 15. 3Sum/C/best_solution.c
@@ -1,39 +1,13 @@
 /**
+ * I looked at solutions in Leetcode to come up with this code.
+ * My blind attempt is the optimal solution, so the changes are mostly for clarity.
+ *
  * Time complexity: O(n^2)
  * Space complexity: O(n)
  * */
 #include "shared.h"
 
-int partition(int* arr, int low, int high) {
-	int pivot = arr[low];
-
-	int i = low - 1;
-	int j = high + 1;
-
-	while(true) {
-		do {
-			++i;
-		} while(arr[i] < pivot);
-
-		do {
-			--j;
-		} while(arr[j] > pivot);
-
-		if(i >= j) return j;
-
-		int temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
-}
-
-void quicksort(int* arr, int low, int high) {
-	if(low >= 0 && high >= 0 && low < high) {
-		int p = partition(arr, low, high);
-		quicksort(arr, low, p);
-		quicksort(arr, p + 1, high);
-	}
-}
+int cmp(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
 
 /**
  * Return an array of arrays of size *returnSize.
@@ -47,34 +21,17 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
 	*returnColumnSizes = (int*)calloc(capacity, sizeof(int));
 	*returnSize = 0;
 
-	if(numsSize == 3) {
-		int sum = nums[0] + nums[1] + nums[2];
-
-		if(sum == 0) {
-			result[0] = (int*)malloc(3 * sizeof(int));
-
-			result[0][0] = nums[0];
-			result[0][1] = nums[1];
-			result[0][2] = nums[2];
-
-			*returnColumnSizes[0] = 3;
-			*returnSize = 1;
-		}
-
-		return result;
-	}
-
-	quicksort(nums, 0, numsSize - 1);
+	qsort(nums, numsSize, sizeof(int), cmp);
 
 	for(int i = 0; i < numsSize - 2; ++i) {
 		// skip duplicate anchor values
 		if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-		int j = i + 1;
-		int k = numsSize - 1;
+		int left = i + 1;
+		int right = numsSize - 1;
 
-		while(j < k) {
-			int sum = nums[i] + nums[j] + nums[k];
+		while(left < right) {
+			int sum = nums[i] + nums[left] + nums[right];
 
 			if(sum == 0) {
 				if(*returnSize == capacity) {
@@ -86,28 +43,28 @@ int** threeSum(int* nums, int numsSize, int* returnSize, int** returnColumnSizes
 				result[*returnSize] = (int*)malloc(3 * sizeof(int));
 
 				result[*returnSize][0] = nums[i];
-				result[*returnSize][1] = nums[j];
-				result[*returnSize][2] = nums[k];
+				result[*returnSize][1] = nums[left];
+				result[*returnSize][2] = nums[right];
 
 				(*returnColumnSizes)[*returnSize] = 3;
 				*returnSize = *returnSize + 1;
 
-				++j;
-				--k;
+				++left;
+				--right;
 
 				// Skip duplicate j values
-				while(j < k && nums[j] == nums[j - 1])
-					++j;
+				while(left < right && nums[left] == nums[left - 1])
+					++left;
 
 				// Skip duplicate k values
-				while(j < k && nums[k] == nums[k + 1])
-					--k;
+				while(left < right && nums[right] == nums[right + 1])
+					--right;
 			}
 			else if(sum < 0) {
-				++j;
+				++left;
 			}
 			else {
-				--k;
+				--right;
 			}
 		}
 	}
