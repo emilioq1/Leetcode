b368e9385b44596ff66b9c99df45bbd4cfece3fd
emilioq1 <quiemili354@gmail.com>
2026-02-27 19:20:45 -0600

    My current implementation works for all the cases, but the last one, so I need to debug

--- 16. 3Sum Closest/C/blind_attempt.c
+++ 16. 3Sum Closest/C/blind_attempt.c
@@ -2,4 +2,77 @@
 
 int cmp(const void* a, const void* b) { return (*(int*)a - *(int*)b); }
 
-int threeSumClosest(int* nums, int numsSize, int target) {}
+int threeSumClosest(int* nums, int numsSize, int target) {
+	if(numsSize == 3) {
+		return nums[0] + nums[1] + nums[2];
+	}
+
+	int result = 0;
+
+	int currentBestDiff = MAX_LENGTH + 1;
+	int currentBestSum = 0;
+
+	qsort(nums, numsSize, sizeof(int), cmp);
+	printIntArray(nums, numsSize);
+	printf("\n");
+
+	for(int i = 0; i < numsSize - 2; ++i) {
+		// skip duplicate anchor values
+		// if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+		int left = i + 1;
+		int right = numsSize - 1;
+
+
+		while(left < right) {
+			printf("i: %d\n", i);
+			printf("left: %d\n", left);
+			printf("right: %d\n", right);
+			printf("nums[i]: %d\n", nums[i]);
+			printf("nums[left]: %d\n", nums[left]);
+			printf("nums[right]: %d\n", nums[right]);
+
+			int sum = nums[i] + nums[left] + nums[right];
+			printf("sum: %d\n", sum);
+
+			int diff = 0;
+			if(sum > target) diff = sum - target;
+			else diff = target - sum;
+
+			printf("diff: %d\n\n", diff);
+
+			if(diff < currentBestDiff) {
+				if(diff == 0) return sum;
+
+				currentBestDiff = diff;
+				currentBestSum = sum;
+
+				printf("currentBestDiff: %d\n", currentBestDiff);
+				printf("currentBest: %d\n", currentBestSum);
+
+				//++left;
+				//--right;
+
+				// Skip duplicate j values
+				// while(left < right && nums[left] == nums[left - 1])
+				//	++left;
+
+				//// Skip duplicate k values
+				// while(left < right && nums[right] == nums[right + 1])
+				//	--right;
+			}
+			if(sum < target) {
+				++left;
+			}
+			else {
+				--right;
+			}
+
+			printf("\n\n");
+		}
+	}
+
+	result = currentBestSum;
+
+	return result;
+}
