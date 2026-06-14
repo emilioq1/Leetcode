2cb64947c71f330c5b8bb547ea9e685380aa372b
emilioq1 <quiemili354@gmail.com>
2026-06-14 09:14:06 -0500

    Finished blind attempt for question 21 and it got accepted in Leetcode

--- 21. Merge Two Sorted Lists/C/blind_attempt.c
+++ 21. Merge Two Sorted Lists/C/blind_attempt.c
@@ -1,4 +1,55 @@
+/*
+ * Time Complexity: O(n^2)
+ * Space Complexity: O(log(n))
+ * */
+
 #include "shared.h"
+
+struct ListNode* getTail(struct ListNode* curr) {
+	while(curr != NULL && curr->next != NULL)
+		curr = curr->next;
+	return curr;
+}
+
+struct ListNode* partition(struct ListNode* head, struct ListNode* tail) {
+	struct ListNode* pivot = head;
+
+	struct ListNode* pre = head;
+	struct ListNode* curr = head;
+
+	while(curr != tail->next) {
+		if(curr->val < pivot->val) {
+			int temp = curr->val;
+			curr->val = pre->next->val;
+			pre->next->val = temp;
+
+			pre = pre->next;
+		}
+		curr = curr->next;
+	}
+
+	int currData = pivot->val;
+	pivot->val = pre->val;
+	pre->val = currData;
+
+	return pre;
+}
+
+void helper(struct ListNode* head, struct ListNode* tail) {
+	if(head == NULL || head == tail) return;
+
+	struct ListNode* pivot = partition(head, tail);
+
+	helper(head, pivot);
+	helper(pivot->next, tail);
+}
+
+
+struct ListNode* quicksort(struct ListNode* head) {
+	struct ListNode* tail = getTail(head);
+	helper(head, tail);
+	return head;
+}
 
 /**
  * Definition for singly-linked list.
@@ -8,5 +59,22 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
-	return NULL;
+	if(list1 == NULL) return list2;
+	if(list2 == NULL) return list1;
+
+	struct ListNode* result = list1;
+
+	int size = 0;
+	struct ListNode* prev = NULL;
+	struct ListNode* curr = list1;
+	while(curr != NULL) {
+		++size;
+		prev = curr;
+		curr = curr->next;
+	}
+	prev->next = list2;
+
+	quicksort(result);
+
+	return result;
 }
